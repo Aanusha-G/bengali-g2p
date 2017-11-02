df30440ac521839dc33bd6e178a06848b8387aae
Aanusha Ghosh <aanusha.ghosh@gmail.com>
2017-11-03 00:09:33 +0100

    stitch.py: Read mappings file into dictionary and output rudimentary phonemes for test word test_bangla: Add xipa transcriptions for test words

--- stitch.py
+++ stitch.py
@@ -1,4 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
 import os
-dir = os.path.dirname(__file__)
-filename = os.path.join(dir, '/relative/path/to/file/you/want')
-curdir = os.getcwd()
+#dir = os.path.dirname(__file__)
+#filename = os.path.join(dir, '/relative/path/to/file/you/want')
+#curdir = os.getcwd()
+with open("/Users/aanusha/bengali-g2p/character_mappings") as f:
+	data = f.readlines()
+
+mappings = {}
+for line in data:
+	character = line.split(' -> ')[0]
+	xipa = line.split(' -> ')[1].strip()
+	mappings[character] = xipa
+
+
+word = unicode('খিচুরি','utf-8')
+word_ipa = []
+for i in word:
+	print i
+	word_ipa.append(mappings[i.encode('utf-8')])
+
+print ''.join(word_ipa)
