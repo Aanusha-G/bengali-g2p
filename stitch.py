531afc0a5b88ce6696ca2517e2747dcd4b003d70
Aanusha Ghosh <aanusha.ghosh@gmail.com>
2017-11-03 21:43:46 +0100

    stitch.py: Add syllabify() function

--- stitch.py
+++ stitch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import os
 #dir = os.path.dirname(__file__)
 #filename = os.path.join(dir, '/relative/path/to/file/you/want')
@@ -20,7 +20,8 @@ def g2p(word):
 	for i in word:
 		word_ipa.append(mappings[i.encode('utf-8')])
 
-	return ''.join(word_ipa)
+	return syllabify(''.join(word_ipa))
+	#return ''.join(word_ipa)
 
 
 def run_tests():
@@ -36,6 +37,29 @@ def run_tests():
 		print "Expected: ", test_pairs[i], "\nG2P: ", g2p_output
 		if test_pairs[i] == g2p_output:
 			print "pass!"
+		else:
+			print "Test failed"
+
+
+def syllabify(word):
+	vowels = ['aa', 'i', 'iy', 'e', 'ei', 'ae', 'o', 'ow', 'ou', 'u', 'uw']
+	ph = word.strip('|').split('|')
+	syl = []
+	v_flag = 0
+	c_flag = 0
+	l = len(ph) - 1
+	for index, i in enumerate(reversed(ph)):
+		syl.append(i+'|')
+		if i in vowels:
+			v_flag = 1
+		else:
+			if v_flag == 1:
+				c_flag = 1
+		if v_flag == 1 and c_flag == 1 and index != l:
+			v_flag = c_flag = 0
+			syl.append('-')
+	syl_word = ''.join(list(reversed(syl)))
+	return syl_word
 
 
 run_tests()
