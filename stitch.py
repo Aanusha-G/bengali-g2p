2a3929d538d1811e3c66ae4593efa05b80cbbf05
Aanusha Ghosh <aanusha.ghosh@gmail.com>
2017-11-05 01:18:23 +0100

    stitch.py: Refine gemination rules (and comment out print statements for passing tests) test_bangla: Add more test_cases

--- stitch.py
+++ stitch.py
@@ -22,19 +22,19 @@ def g2p(word):
 
 	replacements = [
 		# Delete schwa before halant
-		(u'AX\|XX\|', u'XX|'),
+		(r'AX\|XX\|', r'XX|'),
 		# Delete schwa followed by vowel ligatures
-		(u'AX\|(?=(?:aa|i|u|e|ow|oi|ou|)l\|)', u''),
+		(r'AX\|(?=(?:aa|i|u|e|ow|oi|ou|)l\|)', r''),
 		# Retain all first syllable schwas (between non-archiphonemes) - this rule might be redundant: check
 		(r'^[a-z]{1,3}\|\KAX\|(?=[a-z]{1,3}\|)', r'ao|'),
 		# XX|j|aal| -> ae|
 		(r'XX\|j\|aal\|', r'ae|'),
 		# Word-final h is always followed by ow|
-		(u'h\|AX\|$', u'h|ow|'),
+		(r'h\|AX\|$', r'h|ow|'),
 		# Word-final schwas -> ow| for conjugate clusters
 		(r'XX\|\w{1,3}\|\KAX\|$', r'ow|'),
 		# Other word-final schwas are deleted
-		(u'AX\|$', u''),
+		(r'AX\|$', r''),
 		# AX|cons|AX|$ -> ao|cons|
 		(r'AX\|([a-z]{1,3}\|$)', r'ow|\1'),
 		# Alternating schwas -> ow| (after full vowel)
@@ -42,36 +42,42 @@ def g2p(word):
 		# Alternating schwas -> ow| (after implied schwa)
 		(r'AX\|\w{1,3}\|\KAX\|', r'ow|'),
 		# "swa" -> sh|
-		(u'sx\|XX\|(?:b|m)\|', u'sh|'),
+		(r'sx\|XX\|(?:b|m)\|', r'sh|'),
 		# sx+cons -> s|
 		(r'sx\|XX\|', r's|'),
+		# all other sx -> sh|
+		(r'sx\|', r'sh|'),
 		# ligature vowel + y -> ow|
 		(r'(?:(?:aa|i|u|e|ow|oi|ou|)l\|)\Ky\|', r'ow|'),
 		# plosive + j phola -> geminate
-		(r'(p|t|k|b|d|g)\|XX\|j\|', r'\1|\1|'),
+		(r'((?:p|td?|k|b|dd?|g)h?)\|XX\|j\|', r'\1|\1|'),
+		# Clean up aspirated geminates from above step (bh|bh| -> b|bh|)
+		(r'((\w{1,2})h)\|\1\|', r'\2|\1|'),
 		# Handle -tam
 		(r'AX\|(?=td\|aal\|m\|)', r''),
 		# Convert all remaining shwas to ao|
 		# Might want to apply this rule in a separate step after conjugate clusters are sorted out
 		# Or after common morphemes like -tam are sorted out
-		(u'AX\|', u'ao|'),
+		(r'AX\|', r'ao|'),
 		# Remove all remaining halants
-		(u'XX\|', u''),
+		(r'XX\|', r''),
 		# Nasalise vowels
 		(r'(?:(?:aa|ae|ao|i|u|e|ow|oi|ou)l?)\K\|NX\|', r'n|'),
 		# Remove vowel ligature symbols
-		(r'(?:aa|ae|ao|i|u|e|ow|oi|ou)\Kl', r'')
+		(r'(?:aa|ae|ao|i|u|e|ow|oi|ou)\Kl', r''),
+		# ao| or ow| + y| = oy|
+		(r'(ao|ow)\|y\|',r'oy|')
 	]
-	print "phoneme_string before schwa_deletion", phoneme_string
+	#print "phoneme_string before schwa_deletion", phoneme_string
 
 	for old, new in replacements: 
 		phoneme_string = regex.sub(old, new, phoneme_string)
-	print "phoneme_string after schwa_deletion" , phoneme_string
+	#print "phoneme_string after schwa_deletion" , phoneme_string
 	return syllabify(phoneme_string)
 
 
 def syllabify(word):
-	vowels = ['aa', 'ae', 'ao', 'ow', 'ou', 'i', 'e', 'oi', 'u', 'AX']
+	vowels = ['aa', 'ae', 'ao', 'ow', 'ou', 'i', 'e', 'oi', 'u', 'oy', 'AX']
 	ph = word.strip('|').split('|')
 	syl = []
 	v_flag = 0
@@ -91,7 +97,7 @@ def syllabify(word):
 	syl_word = ''.join(list(reversed(syl)))
 	# If first syllable has no vowel, remove first syllable marker
 	first_syll = syl_word.split('-')[0]
-	print first_syll
+	#print first_syll
 	if regex.search(r'(aa|ae|ao|i|e|u|o|oi|ou)', first_syll) == None:
 		syl_word = regex.sub(r'-', r'', syl_word, 1)
 
@@ -111,13 +117,14 @@ def run_tests():
 	c = 0
 	for i in test_pairs.keys():
 		g2p_output = g2p(unicode(i,'utf-8'))
-		print "Expected: ", test_pairs[i], "\nG2P: ", g2p_output
+		#print "Expected: ", test_pairs[i], "\nG2P: ", g2p_output
 		if test_pairs[i] == g2p_output:
-			print "pass!"
+			#print "pass!"
 			c+=1
 		else:
+			print "Expected: ", test_pairs[i], "\nG2P: ", g2p_output
 			print "Test failed"
-		print '________________'
+			print '________________'
 	print c, " out of ", tot_tests, " passed "
 	
 run_tests()
